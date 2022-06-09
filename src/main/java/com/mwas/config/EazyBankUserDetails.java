b7ffee131251da89c3ee75bd4cafbfce344cd14c
ellyjohnmwangi <johnmwangielly@gmail.com>
2022-06-09 12:49:42 +0300

    User security details and configurations added.

---
+++ src/main/java/com/mwas/config/EazyBankUserDetails.java
@@ -0,0 +1,31 @@
+package com.mwas.config;
+
+import com.mwas.model.Customer;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mwas.model.Customer;
+import com.mwas.model.SecurityCustomer;
+import com.mwas.repository.CustomerRepository;
+
+@Service
+public class EazyBankUserDetails implements UserDetailsService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if (customer.size() == 0) {
+            throw new UsernameNotFoundException("User details not found for the user : " + username);
+        }
+        return new SecurityCustomer(customer.get(0));
+    }
+
+}
