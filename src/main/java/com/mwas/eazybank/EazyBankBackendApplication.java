b0c49950a0bde6ae89512de3940681127d45ec27
ellyjohnmwangi <johnmwangielly@gmail.com>
2022-09-21 13:17:14 +0300

    enabled WebSecurity Debug

--- src/main/java/com/mwas/eazybank/EazyBankBackendApplication.java
+++ src/main/java/com/mwas/eazybank/EazyBankBackendApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @ComponentScans({ @ComponentScan("com.mwas.controllers"), @ComponentScan("com.mwas.config")})
 @EnableJpaRepositories("com.mwas.repository")
 @EntityScan("com.mwas.model")
+@EnableWebSecurity(debug = true)
 public class EazyBankBackendApplication {
 
     public static void main(String[] args) {
